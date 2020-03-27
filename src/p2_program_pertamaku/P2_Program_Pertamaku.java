af307de215fb6df78512fe76813e23207a953429
fikri-firmansyah <62703555+fikri-firmansyah@users.noreply.github.com>
2020-03-27 16:28:26 +0700

    Add files via upload

---
+++ src/p2_program_pertamaku/P2_Program_Pertamaku.java
@@ -0,0 +1,18 @@
+package p2_program_pertamaku;
+
+/**
+ *
+ * @author mac
+ * 
+ */
+public class P2_Program_Pertamaku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame ut = new NewJFrame();
+      ut.setVisible(true);
+    }
+    
+}
